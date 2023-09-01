a25ccbdae88dc69698b5533115cd4b591a78e6f3
trobonox <57040351+trobonox@users.noreply.github.com>
2023-09-01 21:40:59 +0200

    chore: add base content

---
+++ astro.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Kanri',
+			social: {
+				github: 'https://github.com/trobonox/kanri',
+				discord: 'https://discord.gg/AVqHrvxB9C'
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+			customCss: ['./src/assets/landing.css']
+		}),
+	],
+
+	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+	image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+});
