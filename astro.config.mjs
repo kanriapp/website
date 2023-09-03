b9d15592617c36ad609364404e612b0f1fb6559f
trobonox <57040351+trobonox@users.noreply.github.com>
2023-09-03 09:59:36 +0200

    feat: add extra styling and effects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,39 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Kanri',
-			logo: {
-				light: '/src/assets/kanri_logo_light.svg',
-				dark: '/src/assets/kanri_logo.svg',
-				replacesTitle: false,
-			},
-			social: {
-				github: 'https://github.com/trobonox/kanri',
-				discord: 'https://discord.gg/AVqHrvxB9C'
-			},
-			editLink: {
-				baseUrl: 'https://github.com/trobonox/kanri-website',
-			},
-			sidebar: [
-				{
-					label: 'Getting started',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'What is Kanri?', link: '/getting-started/what-is-kanri/' },
-					]
-				}
-			],
-			customCss: ['./src/assets/landing.css']
-		}),
-	],
-
-	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-	image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+  integrations: [starlight({
+    title: 'Kanri',
+    logo: {
+      light: '/src/assets/kanri_logo_light.svg',
+      dark: '/src/assets/kanri_logo.svg',
+      replacesTitle: false
+    },
+    social: {
+      github: 'https://github.com/trobonox/kanri',
+      discord: 'https://discord.gg/AVqHrvxB9C'
+    },
+    editLink: {
+      baseUrl: 'https://github.com/trobonox/kanri-website'
+    },
+    sidebar: [{
+      label: 'Getting started',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'What is Kanri?',
+        link: '/getting-started/what-is-kanri/'
+      }]
+    }],
+    customCss: ['./src/assets/landing.css']
+  }), solidJs()],
+  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  }
 });
