c653913e36662c68671cecb62140dba0636a83c3
trobonox <57040351+trobonox@users.noreply.github.com>
2023-10-03 10:17:09 +0200

    feat: update content

---
+++ src/components/StarSection.tsx
@@ -0,0 +1,11 @@
+import type { Component } from "solid-js";
+
+export const StarSection: Component = () => {
+    return (
+        <div class="star-section">
+            <h2>Sounds interesting? Leave a star on GitHub to show your support!</h2>
+            <p>If you want to support the Kanri app, please leave a star ⭐️ on GitHub. This is a side-project that does not make any profit so this is the primary way to support development of a free and open source offline Kanban app for Windows, Linux and Mac.</p>
+            <a href="https://github.com/trobonox/kanri">Star on GitHub</a>
+        </div>
+    )
+}
